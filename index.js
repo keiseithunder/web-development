15ea09b947fd3d16d4593a547b66a3589e821e77
WICHARN RUEANGKHAJORN <wicharn.keisei@gmail.com>
2019-03-24 12:40:06 +0700

    [fix] bug

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ for (let index = 0; index < allInput.length; index++) {
 }
 document.querySelector("select[name='arrange']").addEventListener("change", getData);
 
-const maxIten = 100;
+const maxItem = 100;
 function getData() {
     var start = document.querySelector("input[name='start-date']");
     var end = document.querySelector("input[name='end-date']");
@@ -15,95 +15,138 @@ function getData() {
         item = 4;
         if (checkNull(end.value) && !checkNull(start.value)) {
             var temp = new Date(start.value);
+            if(!checkValidInput(temp)){
+                alert("Your in put is not in a correct format");
+                document.querySelector(".loader").classList.toggle("display-none");
+                return;
+            }
             end = new Date(temp.getTime() + (item * 24 * 60 * 60 * 1000));
-            end = end.getFullYear() + "-" + ((end.getMonth() + 1) < 10 ? ("0" + (end.getMonth() + 1)) : end.getMonth() + 1) + "-" + end.getDate();
+            end = end.getFullYear() + "-" + ((end.getMonth() + 1) < 10 ? ("0" + (end.getMonth() + 1)) : end.getMonth() + 1) + "-" + ((end.getDate()) < 10 ? ("0" + (end.getDate())) : end.getDate());
             start = start.value;
         } else if (!checkNull(end.value) && checkNull(start.value)) {
             var temp = new Date(end.value);
+            if(!checkValidInput(temp)){
+                alert("Your in put is not in a correct format");
+                document.querySelector(".loader").classList.toggle("display-none");
+                return;
+            }
             start = new Date(temp.getTime() - (item * 24 * 60 * 60 * 1000));
-            start = start.getFullYear() + "-" + ((start.getMonth() + 1) < 10 ? ("0" + (start.getMonth() + 1)) : start.getMonth() + 1) + "-" + start.getDate();
+            start = start.getFullYear() + "-" + ((start.getMonth() + 1) < 10 ? ("0" + (start.getMonth() + 1)) : start.getMonth() + 1) + "-" + ((start.getDate()) < 10 ? ("0" + (start.getDate())) : start.getDate());
             end = end.value;
         } else if (checkNull(end.value) && checkNull(start.value)) {
             end = new Date();
             start = new Date(end.getTime() - (item * 24 * 60 * 60 * 1000));
-            end = end.getFullYear() + "-" + ((end.getMonth() + 1) < 10 ? ("0" + (end.getMonth() + 1)) : end.getMonth() + 1) + "-" + end.getDate();
-            start = start.getFullYear() + "-" + ((start.getMonth() + 1) < 10 ? ("0" + (start.getMonth() + 1)) : start.getMonth() + 1) + "-" + start.getDate();
+            end = end.getFullYear() + "-" + ((end.getMonth() + 1) < 10 ? ("0" + (end.getMonth() + 1)) : end.getMonth() + 1) + "-" + ((end.getDate()) < 10 ? ("0" + (end.getDate())) : end.getDate());
+            start = start.getFullYear() + "-" + ((start.getMonth() + 1) < 10 ? ("0" + (start.getMonth() + 1)) : start.getMonth() + 1) + "-" + ((start.getDate()) < 10 ? ("0" + (start.getDate())) : start.getDate());
         } else {
             end = new Date(end.value);
             start = new Date(start.value);
+            if(!checkValidInput(start)||!checkValidInput(end)){
+                alert("Your in put is not in a correct format");
+                document.querySelector(".loader").classList.toggle("display-none");
+                return;
+            }
             if (end - start < 0) {
                 alert("Start date cannot after End date");
+                document.querySelector(".loader").classList.toggle("display-none");
                 return;
             }
             var dayRange = Math.floor((end - start) / 86400000) + 1;
-            if(dayRange>100){
-                item.value = 100;
-                if(arrange === 'From Start'){
-                    end = new Date(start.getTime() + ((item.value - 1) * 24 * 60 * 60 * 1000));
-                }else if(arrange === 'From End'){
-                    start = new Date(end.getTime() - ((item.value - 1) * 24 * 60 * 60 * 1000));
-                }
+            // console.log(dayRange);
+            if (dayRange > maxItem) {
+                alert("day range cannot more tham 100 day")
             }
-            end = end.getFullYear() + "-" + ((end.getMonth() + 1) < 10 ? ("0" + (end.getMonth() + 1)) : end.getMonth() + 1) + "-" + end.getDate();
-            start = start.getFullYear() + "-" + ((start.getMonth() + 1) < 10 ? ("0" + (start.getMonth() + 1)) : start.getMonth() + 1) + "-" + start.getDate();
+            //     item.value = 100;
+            //     if (arrange === 'From Start') {
+            //         end = new Date(start.getTime() + ((item.value - 1) * 24 * 60 * 60 * 1000));
+            //         console.log(start);
+            //         console.log(end);
+            //     } else if (arrange === 'From End') {
+            //         start = new Date(end.getTime() - ((item.value - 1) * 24 * 60 * 60 * 1000));
+            //     }
+            // }
+            end = end.getFullYear() + "-" + ((end.getMonth() + 1) < 10 ? ("0" + (end.getMonth() + 1)) : end.getMonth() + 1) + "-" + ((end.getDate()) < 10 ? ("0" + (end.getDate())) : end.getDate());
+            start = start.getFullYear() + "-" + ((start.getMonth() + 1) < 10 ? ("0" + (start.getMonth() + 1)) : start.getMonth() + 1) + "-" + ((start.getDate()) < 10 ? ("0" + (start.getDate())) : start.getDate());
             item = item.value;
         }
 
     } else if (!checkNull(item.value)) {
         if (item.value < 1) {
+            alert("item cannot be less than 1")
+            document.querySelector(".loader").classList.toggle("display-none");
             return;
         }
-        if(item.value >=100){
-            item.value = 100;
+        if (item.value > maxItem) {
+            alert("Maximum item is "+maxItem);
+            item.value = maxItem;
         }
         if (checkNull(end.value) && !checkNull(start.value)) {
             var temp = new Date(start.value);
+            if(!checkValidInput(temp)){
+                alert("Your in put is not in a correct format");
+                document.querySelector(".loader").classList.toggle("display-none");
+                return;
+            }
             end = new Date(temp.getTime() + ((item.value - 1) * 24 * 60 * 60 * 1000));
-            end = end.getFullYear() + "-" + ((end.getMonth() + 1) < 10 ? ("0" + (end.getMonth() + 1)) : end.getMonth() + 1) + "-" + end.getDate();
+            end = end.getFullYear() + "-" + ((end.getMonth() + 1) < 10 ? ("0" + (end.getMonth() + 1)) : end.getMonth() + 1) + "-" + ((end.getDate()) < 10 ? ("0" + (end.getDate())) : end.getDate());
             start = start.value;
             item = item.value;
         } else if (!checkNull(end.value) && checkNull(start.value)) {
             var temp = new Date(end.value);
+            if(!checkValidInput(temp)){
+                alert("Your in put is not in a correct format");
+                document.querySelector(".loader").classList.toggle("display-none");
+                return;
+            }
             start = new Date(temp.getTime() - ((item.value - 1) * 24 * 60 * 60 * 1000));
-            start = start.getFullYear() + "-" + ((start.getMonth() + 1) < 10 ? ("0" + (start.getMonth() + 1)) : start.getMonth() + 1) + "-" + start.getDate();
+            start = start.getFullYear() + "-" + ((start.getMonth() + 1) < 10 ? ("0" + (start.getMonth() + 1)) : start.getMonth() + 1) + "-" + ((start.getDate()) < 10 ? ("0" + (start.getDate())) : start.getDate());
             end = end.value;
             item = item.value;
         } else if (checkNull(end.value) && checkNull(start.value)) {
             end = new Date();
             start = new Date(end.getTime() - ((item.value - 1) * 24 * 60 * 60 * 1000));
-            end = end.getFullYear() + "-" + ((end.getMonth() + 1) < 10 ? ("0" + (end.getMonth() + 1)) : end.getMonth() + 1) + "-" + end.getDate();
-            start = start.getFullYear() + "-" + ((start.getMonth() + 1) < 10 ? ("0" + (start.getMonth() + 1)) : start.getMonth() + 1) + "-" + start.getDate();
+            end = end.getFullYear() + "-" + ((end.getMonth() + 1) < 10 ? ("0" + (end.getMonth() + 1)) : end.getMonth() + 1) + "-" + ((end.getDate()) < 10 ? ("0" + (end.getDate())) : end.getDate());
+            start = start.getFullYear() + "-" + ((start.getMonth() + 1) < 10 ? ("0" + (start.getMonth() + 1)) : start.getMonth() + 1) + "-" + ((start.getDate()) < 10 ? ("0" + (start.getDate())) : start.getDate());
             item = item.value;
         } else {
             end = new Date(end.value);
             start = new Date(start.value);
+            if(!checkValidInput(start)||!checkValidInput(end)){
+                alert("Your in put is not in a correct format");
+                document.querySelector(".loader").classList.toggle("display-none");
+                return;
+            }
             if (end - start < 0) {
                 alert("Start date cannot after End date");
+                document.querySelector(".loader").classList.toggle("display-none");
                 return;
             }
             var dayRange = Math.floor((end - start) / 86400000) + 1;
-            if(dayRange>100){
-                item.value = 100;
-                if(arrange === 'From Start'){
+            if (dayRange > maxItem&&item.value>maxItem) {
+                item.value = maxItem;
+                if (arrange === 'From Start') {
                     end = new Date(start.getTime() + ((item.value - 1) * 24 * 60 * 60 * 1000));
-                }else if(arrange === 'From End'){
+                } else if (arrange === 'From End') {
                     start = new Date(end.getTime() - ((item.value - 1) * 24 * 60 * 60 * 1000));
                 }
             }
-            end = end.getFullYear() + "-" + ((end.getMonth() + 1) < 10 ? ("0" + (end.getMonth() + 1)) : end.getMonth() + 1) + "-" + end.getDate();
-            start = start.getFullYear() + "-" + ((start.getMonth() + 1) < 10 ? ("0" + (start.getMonth() + 1)) : start.getMonth() + 1) + "-" + start.getDate();
+            end = end.getFullYear() + "-" + ((end.getMonth() + 1) < 10 ? ("0" + (end.getMonth() + 1)) : end.getMonth() + 1) + "-" + ((end.getDate()) < 10 ? ("0" + (end.getDate())) : end.getDate());
+            start = start.getFullYear() + "-" + ((start.getMonth() + 1) < 10 ? ("0" + (start.getMonth() + 1)) : start.getMonth() + 1) + "-" + ((start.getDate()) < 10 ? ("0" + (start.getDate())) : start.getDate());
             item = item.value;
         }
 
     }
-    console.log(start + "--" + end);
-    
-    generateData(start, end, arrange);
+    //console.log(start + "--" + end);
+    if (!checkValidDate(start) || !checkValidDate(end)) {
+        document.querySelector(".loader").classList.toggle("display-none");
+        return;
+    }
+    generateData(start, end, arrange,item);
 
 }
 
 
-function generateData(start, end, whatFirst) {
+function generateData(start, end, whatFirst,item) {
     // var days = 4;
     // var date = new Date();
     // var last = new Date(date.getTime() - (days * 24 * 60 * 60 * 1000));
@@ -112,7 +155,7 @@ function generateData(start, end, whatFirst) {
     // var year = last.getFullYear();
     // var start_date = year + "-" + month + "-" + day;
     // var end_date = date.getFullYear() + "-" + ((date.getMonth() + 1) < 10 ? ("0" + (date.getMonth() + 1)) : date.getMonth() + 1) + "-" + date.getDate();
-    
+
     var request = new XMLHttpRequest()
     // Open a new connection, using the GET request on the URL endpoint
     if (start !== end) {
@@ -129,7 +172,7 @@ function generateData(start, end, whatFirst) {
             generateCard(data);
         }
         if (whatFirst === 'From Start') {
-            for (let index = 0; index < data.length; index++) {
+            for (let index = 0; index < data.length&&index<maxItem&&index<item; index++) {
                 if (index % 3 == 0) {
 
                     generateRow();
@@ -137,19 +180,19 @@ function generateData(start, end, whatFirst) {
                 generateCard(data[index]);
             }
             document.querySelector(".loader").classList.toggle("display-none");
-        }else if(whatFirst === 'From End'){
-            for (let index = 0; index < data.length; index++) {
+        } else if (whatFirst === 'From End') {
+            for (let index = 0; index < data.length&&index<maxItem&&index<item; index++) {
                 if (index % 3 == 0) {
                     generateRow();
                 }
-                generateCard(data[data.length-index-1]);
+                generateCard(data[data.length - index - 1]);
             }
             document.querySelector(".loader").classList.toggle("display-none");
         }
-        
+
         console.log(data);
     }
-    
+
     // Send request
     request.send()
 }
@@ -176,4 +219,17 @@ function checkNull(value) {
         return true;
     }
     return false;
+}
+function checkValidDate(date) {
+    var tempp = new Date();
+    if (Math.floor((tempp - date) / 86400000) < 0) {
+        return false;
+    }
+    return true;
+}
+function checkValidInput(date){
+    if(date == 'Invalid Date'){
+        return false;
+    }
+    return true;
 }
